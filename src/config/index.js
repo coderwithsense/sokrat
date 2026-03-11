644e69308f5f1e0fb837e8f55e1cf80c6793a6cc
Himanshu <himanshupoptani12@gmail.com>
2026-03-12 01:40:49 +0530

    index.js

---
+++ src/config/index.js
@@ -0,0 +1,16 @@
+import fs from 'fs/promises'
+import os from 'os'
+import path from 'path'
+
+export const readConfig = async () => {
+    try {
+        const config = await fs.readFile(path.join(os.homedir(), '.hint-agent', 'config.json'), 'utf-8')
+        if (!config) {
+            throw new Error('NO_CONFIG_FILE')
+        }
+        const parsedConfig = JSON.parse(config)
+        return parsedConfig;
+    } catch (e) {
+        throw new Error('hint-agent not initialized. Run `hint init` first.')
+    }
+}
